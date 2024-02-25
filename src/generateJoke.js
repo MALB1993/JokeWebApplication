0475d05bad17d52a067efa2b50d8cb8c34b4ed9f
MALB1993 <mostafaalaeddin570@gmail.com>
2024-02-25 01:42:13 -0800

    fixed generate joke

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,7 +1,17 @@
-function generateJoke()
-{
-    return "Die einen verstehen das Binäre System, die anderen nicht.";
-}
+import axios from "axios";
 
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    axios.get("https://icanhazdadjoke.com", config).then((res) => {
+        document.getElementById('joke').innerHTML = res.data.joke;
+    }).catch((error) => {
+        console.error('Error fetching joke:', error);
+    });
+}
 
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 import generateJoke from "./generateJoke";
 //=================================================> Import main sass file
 import './style/main.scss';
+//=================================================> Import laughing
+// import langhig from "./assets/laughing.svg";
 
-console.log(generateJoke());
+
+// const langhig = document.getElementById('langhig');
+// langhig.src = langhig;
+
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
