cc5b9912fe73857c4d417122db701ff01cc1237a
MALB1993 <mostafaalaeddin570@gmail.com>
2024-02-25 00:26:22 -0800

    set comments and import sass file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//=================================================> Import generate jone module
 import generateJoke from "./generateJoke";
+//=================================================> Import main sass file
+import './style/main.scss';
 
 console.log(generateJoke());
