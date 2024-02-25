f24e4e9d5d4f8135dfe0caccf4ba5e3cd8606a3d
MALB1993 <mostafaalaeddin570@gmail.com>
2024-02-25 02:01:29 -0800

    fixed joke structure

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@
 import generateJoke from "./generateJoke";
 //=================================================> Import main sass file
 import './style/main.scss';
-//=================================================> Import laughing
-// import langhig from "./assets/laughing.svg";
-
-
-// const langhig = document.getElementById('langhig');
-// langhig.src = langhig;
-
+//=================================================> Import Boostrap 
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 const jokeBtn = document.getElementById('jokeBtn');
 jokeBtn.addEventListener('click', generateJoke);
